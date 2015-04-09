36d88b2d23c9cea3aa52b491369d097ac0b3cf4c
David Harrison Smith <exmokie@gmail.com>
2015-04-08 18:28:55 -0600

    added warmups and exercises

--- python-exercises/python-exercises/exercise_3.py
+++ python-exercises/python-exercises/exercise_3.py
@@ -11,20 +11,9 @@ yards_per_mile = 1760
 meters_per_hour = mph * 1609.34
 feet_per_hour = mph * 5280
 
-meters_per_day = meters_per_hour * 24
-barleycorns_per_day = meters_per_day * 117.647
-
-yards_per_hour = 1760 * mph
-furlongs_per_hour = yards_per_hour = yards_per_hour / 220
-furlongs_per_day = furlongs_per_hour * 24
-furlongs_per_week = furlongs_per_day * 7
-
-mach = (feet_per_hour / 60 / 60) / 1130
-
-psl = ((meters_per_hour / 60 / 60) / 299792458) * 100
 
 print "Original speed in miles/hour is %.2f" % (mph)
 print "Converted to barleycorn/day is: %.2f" % (barleycorns_per_day)
 print "Converted to furlongs/fortnight is: %.2f" % (furlongs_per_week * 2)
-print "Converted to Mach number is %f:" % (mach)
+print "Converted to Mach number is:" % (mach)
 print "Converted to the percentage of the speed of light is: ", psl
